60b79ad27645f70ba31b52e5dbd0e3906d54cf64
itachi-ems <jshahharsh@gmail.com>
2021-07-04 15:10:23 +0530

    minor changes in api/user

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const app = express();
 
 app.use(express.json());
 
+
+routes(app);
+
 app.use("/",(req,res)=>{
     res.sendStatus(200);
 })
-routes(app);
 
 const port = process.env.port || 3000;
 app.listen(port,()=>{
 
     console.log("Server Is Running");
 });
+
